e514a0f554912ad641eb52be36315bf554fe474d
ccbteusml-svg <ccb.teus.ml@gmail.com>
2026-05-12 09:48:18 -0400

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// supabase-config.js - Arquivo central de conexão com o banco de dados
-const supabaseUrl = 'https://qrctbkgmztiebluiyzys.supabase.co';
-const supabaseKey = 'sb_publishable_SoS2YOc2Xr2wZwn8rTaUYA_va1LQi0h'; 
-
-// Cria a conexão e deixa ela disponível para o aplicativo inteiro
-var supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
